9bb8ba325ccf0f7c417135c501f7f9e454857078
이소윤 <soyoon@isoyuns-MBP.fc.ca>
2019-11-04 17:22:19 -0500

    november fifth

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,60 @@
 const myVM = (() => {
     // get the user buttons and fire off an async DB query with Fetch
 
+let userButtons = document.querySelectorAll('.u-link');
+let lightBox = document.querySelector('.lightbox');
+
+function getUserData(event){
+    event.preventDefault();
+    //1,2, or 3 depending on which achor tag you click
+    let url = `/users/${this.getAttribute('href')}`;
+    let currentImg = this.previousElementSibling.getAttribute('src');
+
+    //this goes and fetches the database content (or an API endpoint)
+    //that's why its called a fetch
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            data.currentSrc = currentImg;
+            parseUserData(data);
+        })
+        .catch((err) => console.log(err));
+};
+
+
+function renderSocialMedia(media){
+    return `<ul class="u-social">
+       ${media.map(item =>`<li>${item}</li>`).join("")}</ul>`
+       
+};
+
+
+
+    function parseUserData(person) {
+        let targetDiv = lightBox.querySelector('.lb-content');
+        let targetImg = lightBox.querySelector('img');
+        let bioContent = `
+        <p>${person.bio}</p>
+        <h4>Social Media:</h4>
+        <!-- loop thru social media stuff here -->   
+         ${renderSocialMedia(person.social)}     
+        `;
+
+        targetDiv.innerHTML = bioContent;
+        targetImg.src = person.currentSrc;
+        lightBox.classList.add('show-lb');
+
+    }
+
+  
+
+
+    userButtons.forEach(button => button.addEventListener("click" , getUserData ));
+
+    lightBox.querySelector('.close').addEventListener("click", function(){
+        lightBox.classList.remove('show-lb');
+    });
+
 })();
--- routes/index.js
+++ routes/index.js
@@ -16,24 +16,35 @@ router.get('/', (req, res) => {
 
         // render the home view with dynamic data
         res.render('home', { people: result });
+        
     })
 })
 
 
-router.get('/:id', (req, res) => {
+router.get('/users/:id', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the user route');
     console.log(req.params.id); // 1,2 3 whatever comes after the slash
 
-    let query = `SELECT * FROM tbl_bio WHERE ID="${req.params.id}"`;
+    let query = `SELECT * FROM tbl_bio WHERE profID="${req.params.id}"`;
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
         console.log(result); // should see objects wrapped in an array
 
+        //convert the social property into an array
+
+        //map is an array method that lets u map one value to another (convert it)
+        result[0].social = result[0].social.split(",").map(function(item) {
+            item = item.trim();
+            return item;
+        })
+
+        console.log("after trim / conversion:", result[0]);
+
         // render the home view with dynamic data
-        //res.render('home', { people: result });
+        res.json(result[0]);
     })
 })
 
